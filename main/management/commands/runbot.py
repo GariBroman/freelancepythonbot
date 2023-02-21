d97373d4f238134577aaa8d122350fe210c4d9b1
ilyashirko <ilyashirko@gmail.com>
2023-02-21 20:23:24 +0300

    init project

---
+++ main/management/commands/runbot.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from environs import Env
+
+
+
+
+class Command(BaseCommand):
+    help = "Start Telegram bot"
+
+    def handle(self, *args, **kwargs):
+
+        env = Env()
+        env.read_env()
