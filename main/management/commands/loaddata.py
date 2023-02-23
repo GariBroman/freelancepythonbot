f449ac175f66d9cb6cdd1029fb16b9569aab7e6e
kruser <kruser@yandex.ru>
2023-02-23 18:35:46 +0500

    added loaddata for first start

---
+++ main/management/commands/loaddata.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from main.models import ExampleOrder, Tariff
+from django.utils.timezone import timedelta
+
+
+TARIFF_ITEMS = [
+    {
+        'title': 'Эконом',
+        'orders_limit': 5,
+        'answer_delay': timedelta(hours=24)
+    },
+    {
+        'title': 'Стандарт',
+        'orders_limit': 15,
+        'answer_delay': timedelta(hours=1),
+        'personal_contractor_available': True
+    },
+    {
+        'title': 'VIP',
+        'orders_limit': 60,
+        'answer_delay': timedelta(hours=1),
+        'contractor_contacts_availability': True
+    },
+]
+
+EXAMPLE_TEXT = [
+    'Здравствуйте, нужно добавить в интернет - магазин фильтр товаров по цвету',
+    'Здравствуйте, нужно выгрузить товары с сайта в Excel - таблице',
+    'Здравствуйте, нужно загрузить 450 SKU на сайт из Excel таблицы',
+    'Здравствуйте, хочу провести на сайте акцию, хочу разместить баннер и добавить функционал, чтобы впридачу к акционным товарам выдавался приз',
+]
+
+class Command(BaseCommand):
+    help = "Загрузка начальных данных"
+
+    def handle(self, *args, **kwargs):
+
+        if not ExampleOrder.objects.all():
+            ExampleOrder.objects.bulk_create([ExampleOrder(text=text) for text in EXAMPLE_TEXT])
+
+        if not Tariff.objects.all():
+            tariffs = [Tariff(**item) for item in TARIFF_ITEMS]
+            Tariff.objects.bulk_create(tariffs)
+
