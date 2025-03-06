2761909fab35135a9a213edc499871c6d4258373
GariBroman <your.email@example.com>
2025-03-06 19:23:06 +0300

    Добавлена документация и настройки для медиа-файлов

--- osminog/settings.py
+++ osminog/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files (Uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- osminog/urls.py
+++ osminog/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import reverse
 from main.views import yookassa_webhook
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def redirect2admin(request):
@@ -12,3 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhook/yookassa/', yookassa_webhook, name='yookassa_webhook'),
 ]
+
+# Добавляем обработку медиа-файлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
